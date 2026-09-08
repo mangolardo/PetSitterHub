2ab17d9c65c3b11f64883d7f242e8754e2bb25e1
lardo <lardo.mango.pyxxa@gmail.com>
2026-09-08 16:19:23 +0200

    migliori professionisti, sistemato login/logout/area personale

--- backend/routing/serviceRouter.js
+++ backend/routing/serviceRouter.js
@@ -5,10 +5,11 @@ const servicesHandler = require('./handlers/serviceHandler.js');
 const verifyToken = require('./authMiddleware')
 const serviceHandler = require("./handlers/serviceHandler");
 
-
+router.get('/zones',serviceHandler.getZones)
 router.get('/:id', serviceHandler.services)
 router.post('/', verifyToken, serviceHandler.addService)
 router.delete('/:id', verifyToken, serviceHandler.deleteService);
 router.get('/service/:id_service',servicesHandler.getService)
+
 
 module.exports = router;
