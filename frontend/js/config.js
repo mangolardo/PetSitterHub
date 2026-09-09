cd06dfd039f0c259ae14052634137c615df547d8
lardo <lardo.mango.pyxxa@gmail.com>
2026-09-09 18:31:07 +0200

    DEPLOY

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
-//const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ||  window.location.hostname  === '';
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ||  window.location.hostname  === '';
 
-//const API_BASE_URL = isLocalhost
-   // ? 'http://localhost:3000/api'
-//    : '__BACKEND_URL__/api';
-window.API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = isLocalhost
+ ? 'http://localhost:3000/api'
+   : '__BACKEND_URL__/api';
+//window.API_BASE_URL = 'http://localhost:3000/api';
